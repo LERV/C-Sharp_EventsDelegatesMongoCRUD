e5c9871e386289fa152dc8c7483dbbca263fdd11
Edward Rodriguez <lrodriguez@konradgroup.com>
2019-03-29 14:48:03 -0600

    New thread querying mongo DB ready

--- ConnectToMongo/Program.cs
+++ ConnectToMongo/Program.cs
@@ -3,6 +3,7 @@
 
 using MongoDB.Bson;
 using MongoDB.Driver;
+using System.Threading;
 
 namespace ConnectToMongo
 {
@@ -11,10 +12,15 @@ namespace ConnectToMongo
 
     class Program
     {
+        private static bool keepRunning = true;
         static void Main(string[] args)
-        {
-            InsertAsync();
-            getAsync();
+        {            
+
+
+            Thread t = new Thread(new ThreadStart(getAsync));
+            t.Start();
+            //Task
+            //getAsync();
 
 
             //Delegates example
@@ -34,37 +40,51 @@ namespace ConnectToMongo
             delegate1(15);
 
             //NOTE: If using return values with multiple deletes only the last delegate return value is returned
-            Console.WriteLine("");
+            Console.WriteLine("-----------------------------------------");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Write name: ");
+                String name = Console.ReadLine();
+                InsertAsync(name);
+                
+            }       
 
 
             Console.ReadKey();
         }
 
-        static async void InsertAsync()
+        static async void InsertAsync(String name)
         {
             Console.WriteLine("Connecting and Inserting Mongo!");
             var client = new MongoClient("mongodb://localhost:27017");
             var database = client.GetDatabase("schools");
             var collection = database.GetCollection<Reading>("students");
 
-            await collection.InsertOneAsync(new Reading { name = "Smith", lastname="Titan" } );
+            await collection.InsertOneAsync(new Reading { name = name, lastname="Janzing" } );
         }
 
-        static async void getAsync()
+        static void getAsync()
         {
             Console.WriteLine("Connecting and Querying Mongo!");
             var client = new MongoClient("mongodb://localhost:27017");
             var database = client.GetDatabase("schools");
-            var collection = database.GetCollection<Reading>("students");            
+            var collection = database.GetCollection<Reading>("students");
 
-            var list = await collection.Find(_ => true).ToListAsync();
-
-            Console.WriteLine("Printing DB results");
-            foreach (var document in list)
+            while(keepRunning)
             {
-                Console.WriteLine(document.name + " " + document.lastname);
+                Console.WriteLine("*************************Readings********************");
+                //var list = await collection.Find(_ => true).ToListAsync();
+                var list = collection.Find(_ => true).ToList();
+
+                Console.WriteLine("Printing DB results");
+                foreach (var document in list)
+                {
+                    Console.WriteLine(document.name + " " + document.lastname);
+                }
+                Console.WriteLine("**********************************************");
+                Thread.Sleep(5000);
             }
-            Console.WriteLine("");
         }
 
 
